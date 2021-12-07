a55c1d62362e49cc74cb72b48cd2cbdb79a6de32
AlexNecakov <alexnecakov@gmail.com>
2021-12-07 17:47:03 -0500

    scenetwo

--- PA4/SceneFour.py
+++ PA4/SceneFour.py
@@ -20,40 +20,7 @@ import GLUtility
 from DisplayableCube import DisplayableCube
 from DisplayableTorus import DisplayableTorus
 from DisplayableSphere import DisplayableSphere
-
-##### TODO 1: Generate Triangle Meshes
-# Requirements:
-#   1. Use Element Buffer Object (EBO) to draw the cube. The cube provided in the start code is drawn with Vertex Buffer
-#   Object (VBO). In the DisplayableCube class draw method, you should switch from VBO draw to EBO draw. To achieve
-#   this, please first read through VBO and EBO classes in GLBuffer. Then you rewrite the self.vertices and self.indices
-#   in the DisplayableCube class. Once you have all these down, then switch the line vbo.draw() to ebo.draw().
-#   2. Generate Displayable classes for an ellipsoid, torus, and cylinder with end caps.
-#   These classes should be like the DisplayableCube class and they should all use EBO in the draw method.
-#   PS: You must use the ellipsoid formula to generate it, scaling the Displayable sphere doesn't count
-#
-#   Displayable object's self.vertices numpy matrix should be defined as this table:
-#   Column | 0:3                | 3:6           | 6:9          | 9:11
-#   Stores | Vertex coordinates | Vertex normal | Vertex Color | Vertex texture Coordinates
-#
-#   Their __init__ method should accept following input
-#   arguments:
-#   DisplayableEllipsoid(radiusInX, radiusInY, radiusInZ, slices, stacks)
-#   DisplayableTorus(innerRadius, outerRadius, nsides, rings)
-#   DisplayableCylinder(endRadius, height, slices, stacks)
-#
-
-##### TODO 5: Create your scenes
-# Requirements:
-#   1. We provide a fixed scene (SceneOne) for you with preset lights, material, and model parameters.
-#   This scene will be used to examine your illumination implementation, and you should not modify it.
-#   2. Create 3 new scenes (can be static scenes). Each of your scenes must have
-#      * at least 3 differently shaped solid objects
-#      * each object should have a different material
-#      * at least 2 lights
-#      * All types of lights should be used
-#   3. Provide a keyboard interface that allows the user to toggle on/off each of the lights in your scene model:
-#   Hit 1, 2, 3, 4, etc. to identify which light to toggle.
-
+from DisplayableCylinder import DisplayableCylinder
 
 class SceneFour(Component):
     shaderProg = None
